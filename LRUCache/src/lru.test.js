0a2e4eab575c60e36965e8f5446cce02ddf08221
staka625 <takasum88@outlook.jp>
2022-06-21 18:50:02 +0900

    [Add] 先頭のデータを削除するテストケース追加

--- LRUCache/src/lru.test.js
+++ LRUCache/src/lru.test.js
@@ -22,3 +22,27 @@ test("要素を追加する", () => {
     i++;
   });
 });
+
+test("先頭のデータを削除する", () => {
+  /************** generate phase **************/
+  const lru = new LRUCache(2); //キャッシュサイズ1で作成
+  lru.set(1, 1); //[1,1]を追加
+  lru.set(2, 2); //[1,1]を追加
+  lru.deleteFirstElement();
+  console.log(lru); //確認のため表示
+
+  /************** test phase **************/
+  expect(lru.cachesize).toBe(2); //キャッシュサイズは1か？
+  expect(lru.size).toBe(1); //サイズは1か？
+
+  var i = 0;
+  lru.forEach((key, val) => {
+    switch (i) {
+      case 0: //index-0番目のデータがkey,valともに2か？
+        expect(key).toBe(2);
+        expect(val).toBe(2);
+        break;
+    }
+    i++;
+  });
+});
