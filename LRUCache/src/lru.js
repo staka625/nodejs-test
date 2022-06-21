22786315534741366202ea5ea139b4806a0df33e
staka625 <takasum88@outlook.jp>
2022-06-21 18:52:52 +0900

    [Add] 先頭の要素を削除する関数を追加

--- LRUCache/src/lru.js
+++ LRUCache/src/lru.js
@@ -6,6 +6,16 @@ class LRUCache extends Map {
 
   set(key, value) {
     super.set(key, value);
+
+    return this;
+  }
+
+  //最初の要素を削除
+  deleteFirstElement() {
+    for (const elm of this) {
+      this.delete(elm[0]);
+      return elm;
+    }
   }
 }
 
